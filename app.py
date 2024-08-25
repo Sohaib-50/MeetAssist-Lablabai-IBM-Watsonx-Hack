4d234a62a4bf1ed8ca104635605531a9251b2c0c
Sohaib-50 <sohaibahmedabbasi2@gmail.com>
2024-08-25 20:07:10 +0500

    final hack version

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, render_template, request
 from utils.helpers import get_welcome_message, pre_meeting_analysis, post_meeting_analysis
+import os
+import random
 
 app = Flask(__name__)
 
@@ -17,7 +19,19 @@ def pre_meeting():
 
 @app.route('/post-meeting')
 def post_meeting():
-    return render_template('post_meeting.html')
+
+    transcript_files = {}
+    for filename in os.listdir('Transcripts'):
+        if filename.endswith('.txt') and filename.startswith('QMSum'):
+            with open(f'Transcripts/{filename}', 'r') as file:
+                transcript_files[filename] = file.read()
+
+    # get 5 random transcript files
+    # transcript_files = dict(random.sample(transcript_files.items(), 5))
+    transcript_files = dict(random.sample(list(transcript_files.items()), 5))
+
+
+    return render_template('post_meeting.html', transcript_files=transcript_files)
 
 
 @app.route('/pre-meeting-analysis-result', methods=['POST'])
@@ -38,11 +52,16 @@ def pre_meeting_analysis_result():
 def post_meeting_analysis_result():
 
     # get post variables (meeting transcript text file and analysis_type)
-    transcript_file = request.files['meeting_transcript']
+    # transcript_file = request.files['meeting_transcript']
+    transcript_file = request.files.get('meeting_transcript')
+    sample_file_name = request.form.get('sample_file')
     analysis_type = request.form['analysis_type']
-    
-    # convert transcript file to text
-    transcript_text = transcript_file.read().decode('utf-8')
+
+    if sample_file_name:
+        with open(f'Transcripts/{sample_file_name}', 'r') as file:
+            transcript_text = file.read()
+    else:
+        transcript_text = transcript_file.read().decode('utf-8')
 
     result, analysis_title = post_meeting_analysis(transcript_text, analysis_type)
 
